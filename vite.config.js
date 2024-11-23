3396b973e5e931af6ed9d13fdc873116a034bf72
Daniel Mateo Gonzales <mateogonzales1103@gmail.com>
2024-11-23 19:50:56 +1000

    11231950Commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    
+});
